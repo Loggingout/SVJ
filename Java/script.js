4536ec6777980061cfd1364787ccffdf4033a892
INEEDMONEY$ <167272973+Loggingout@users.noreply.github.com>
2025-08-22 09:25:49 +0000

    Added broken slide animation to service container, have to add js to it for it work as intended. Responsive animation is non exisitant right now.

---
+++ Java/script.js
@@ -0,0 +1,6 @@
+// Service Container Animation 
+let serviceContainer = document.getQuerySelector('service-content-container');
+//Looping function
+function animationLoop() {}
+
+//Eventually Form Js function will go here
